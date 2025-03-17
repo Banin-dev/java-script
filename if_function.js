26c66f64b6d0f8284cf281d79db1bfc036c0e5a3
Banin-dev <naziabhatti161@gmail.com>
2025-03-17 13:41:46 +0500

    add more files

---
+++ if_function.js
@@ -0,0 +1,251 @@
+
+   //program 1
+
+{
+    let a = 10;
+     b = 20;
+     if (a + b == 30) {
+        console.log("You are able to do this function");
+    } else {
+         console.log("you are not able to do this function");
+    }
+}
+
+    //program 2
+
+{
+    let c = 20;
+        d = 6;
+    if  (c - d == 40) {
+        console.log("You are prefect driver");
+    } else {
+        console.log("You are not a perfect driver");
+    }    
+}   
+
+    // program 3
+
+{
+     let c = 20;
+        d = 6;
+    if  (c * d == 40) {
+        console.log("It is correct");
+    } else {
+         console.log("It is not corect");
+    }    
+}    
+   
+     // program 4
+
+{
+    let c = 20;
+       d = 6;
+   if  (c / d == 40) {
+       console.log("This division is correct");
+   } else {
+        console.log("This division is not correct");
+   }    
+}
+
+     // program 5
+
+{
+    let c = 20;
+         d = 6;
+    if  (c % d == 40) {
+        console.log("This remindar is correct");
+    } else {
+         console.log("This reminder is not correct");
+    }    
+}
+
+//logical opertors
+
+//AND &&
+
+// program-1
+{ 
+    let  g = 3;
+         h = 5;
+    if (g + h >= 8 && g - h == -2) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }     
+}
+
+// program-2
+{ 
+    let  a = 6;
+         b = 8;
+    if (a + b <= 8 && a - b >= -2) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }     
+}
+
+// program-3
+{ 
+    let a = 5;
+        b = 5;
+    if (a + b > 4 == 10 && a + b == 10){
+        console.log("Today feel powerful");
+    } else {
+        console.log("Today i not feel powerful");
+    }     
+}
+
+// program-4
+{ 
+    let c = 9;
+        d = 4;
+    if (c % d > 4 <= 10 && c / d >= 10){
+        console.log("Sun is rising");
+    } else {
+        console.log("Sun is noy rising");
+    }     
+}
+
+// program-5
+{ 
+    let e = 5;
+        f = 6;
+    if (e * f > 2 <= 12 && e % f >= 10){
+        console.log("Logical operators is start");
+    } else {
+        console.log("logical operator is not start");
+    }     
+}
+
+// program-6
+{ 
+    let u = 6;
+        h = 2;
+    if (u + h == 8 && u - h == 4){
+        console.log("true");
+    } else {
+        console.log("fasle");
+    }     
+}
+
+//OR
+
+
+// program-1
+{ 
+    let  a = 4;
+         b = 4;
+    if ( a + b == 8 || a - b == 4){
+        console.log("true");
+    } else {
+        console.log("fasle");
+    }     
+}
+
+
+// program-2
+{ 
+    let  a = 9;
+         b = 7;
+    if ( a * b <= 8 || a / b >= 4){
+        console.log("true");
+    } else {
+        console.log("fasle");
+    }     
+}
+
+
+// program-3
+{ 
+    let e = 4;
+        f = 6;
+    if ( e % f <= 8 || a * b >= 4){
+        console.log("true");
+    } else {
+        console.log("fasle");
+    }     
+}
+
+
+// NOT
+
+program-1
+{ 
+    let e = 6;
+      
+    if (!ture){
+        console.log("true");
+    } else {
+        console.log("fasle");
+    }     
+}
+
+
+         //nested if
+
+ //program - 1
+
+{
+    let a = 20;
+        b = 30;
+        c = 40;
+     
+    if(a == 20){
+        console.log("the value is correct");
+        if (b == 30) {
+            console.log("the value is correct");
+            if(c == 30 ){
+                console.log("correct value");
+            } else{
+                console.log("incorrect number");
+            }
+        } else{
+            console.log("value is worng");
+        }
+    }  else{
+        console.log("value is wrong");
+    }  
+}
+
+
+
+{
+    let a = 345267886;
+    if(a == 121376879){
+        console.log("You are perfect driver");
+    } else{
+        console.log("You are not a perfect driver");
+    }
+}
+
+
+// program - 3
+
+{
+    let email = "iummulzahra010@gmail.com";
+        pasward = 789123;
+        username = "Ummul";
+    if(email == "iummulzahra010@gmail.com"){
+        console.log("Email is correct");
+        if(pasward == 789123){
+            console.log("passward is correct"); 
+        }
+    }    
+}
+
+// program -4
+
+{
+    let a = 20;
+        b = 36;
+    if( a == 30){
+        console.log("the statement is true");
+        if(b = 40){
+            console.log("numbers are correct");
+        } else{
+            console.log("incorrect value");
+        }
+    }    
+}
+
---
+++ script.js
@@ -0,0 +1,81 @@
+{
+    let a = 3;
+        b = 4;
+    console.log(a + b);    
+}
+
+{ 
+    let myName;
+    console.log(myName);
+}
+
+{
+    let myAge = 21;
+    console.log(myAge);
+    console.log(typeof 21);
+}
+        
+        // operstions data type
+
+        // arithmetic
+{
+let a = 15, b = 10
+console.log(a + b);    //addition
+console.log(a - b);    //subtarction   
+console.log(a * b);    //multplication
+console.log(a % b);    //rimander
+console.log(a / b);    //division
+console.log(a ** b);   //power off
+}     
+
+     //asignment 
+{
+let x = 20;   
+x += 5;       //addition
+console.log(x);
+x -= 10;       //subtraction
+console.log(x) ;  
+x *= 5;         //multiplication
+console.log(x) ; 
+x /= 7;         //division
+console.log(x) ; 
+x %= 9;        //reminder
+console.log(x) ; 
+}   
+
+    //logical operation
+
+{
+    let
+}
+
+
+    //  bitwise
+
+{
+    let
+} 
+
+    //ternary 
+
+{
+    let
+} 
+
+
+    //type operation
+{
+    let
+} 
+
+
+    //nullish 
+{
+    let
+}
+
+
+    //optional
+{
+    let
+}    
