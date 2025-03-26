56f06540b47f07a929b8778e1c1e9bdddd41c03e
Banin-dev <naziabhatti161@gmail.com>
2025-03-26 13:31:27 +0500

    add one file

---
+++ for-loop.js
@@ -0,0 +1,21 @@
+  // loops
+//for loop 
+for(let i = 1 ; i < 5 ; i++) {
+    console.log(i);
+}
+
+// while loop
+{let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+}
+
+// do while loop
+{let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+}
